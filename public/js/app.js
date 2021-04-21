c3e196d36b015e0f51475a23702c70629538eccd
Irma Tobing <irmatobing946@gmail.com>
2021-04-21 22:48:32 +0700

    integrasi front end

--- public/js/app.js
+++ public/js/app.js
@@ -37410,8 +37410,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\kelompok-6\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\kelompok-6\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! E:\Project\kelompok6\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! E:\Project\kelompok6\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
