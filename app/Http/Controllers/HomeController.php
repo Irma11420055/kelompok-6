7d27af602b32f568565ec68df9095b6b0d22930a
simon061901 <82886937+simon061901@users.noreply.github.com>
2021-05-15 22:04:03 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,26 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\BukuModel;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function home()
     {
-        $this->middleware('auth');
+        $bukus = BukuModel::all();
+        return view('frontend/home')->with('bukus',$bukus);
     }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        return view('home');
+    public function adminHome(){
+        return view('administrator/home');
+    }
+    public function komik(){
+        $bukus = DB::table('buku_models')->where('kategory_id','LIKE','Komik')
+        ->get();
+        return view('frontend/komik')->with('bukus',$bukus);
+    }
+    public function novel(){
+        $bukus = DB::table('buku_models')->where('kategory_id','LIKE','Novel')
+        ->get();
+        return view('frontend/novel')->with('bukus',$bukus);
+    }
+    public function ensiklopedia(){
+        $bukus = DB::table('buku_models')->where('kategory_id','LIKE','Ensiklopedia')
+        ->get();
+        return view('frontend/ensiklopedia')->with('bukus',$bukus);
+    }
+    public function cerpen(){
+        $bukus = DB::table('buku_models')->where('kategory_id','LIKE','Cerpen')
+        ->get();
+        return view('frontend/cerpen')->with('bukus',$bukus);
+    }
+    public function dongeng(){
+        $bukus = DB::table('buku_models')->where('kategory_id','LIKE','Dongeng')
+        ->get();
+        return view('frontend/dongeng')->with('bukus',$bukus);
+    }
+    public function biografi(){
+        $bukus = DB::table('buku_models')->where('kategory_id','LIKE','Biografi')
+        ->get();
+        return view('frontend/biografi')->with('bukus',$bukus);
+    }
+    public function contact(){
+        return view('frontend/contact');
+    }
+    public function about(){
+        return view('frontend/about');
     }
 }
